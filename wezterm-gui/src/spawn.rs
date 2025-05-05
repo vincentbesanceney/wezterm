6a23d45a06c675589f351940a3ec346f7dac4d7c
Vincent Besanceney <vincent@b9company.fr>
2025-05-05 07:01:04 +0200

    fixup! fix: honor environment variables and cwd unconditionally for SpawnCommand

--- wezterm-gui/src/spawn.rs
+++ wezterm-gui/src/spawn.rs
@@ -68,19 +68,26 @@ pub async fn spawn_command_internal(
         None
     };
 
-    let cmd_builder = {
-        let mut builder = if let Some(args) = spawn.args {
-            CommandBuilder::from_argv(args.iter().map(Into::into).collect())
-        } else {
-            CommandBuilder::new_default_prog()
-        };
-        for (k, v) in spawn.set_environment_variables.iter() {
-            builder.env(k, v);
+    let cmd_builder = match (
+        spawn.args.as_ref(),
+        spawn.cwd.as_ref(),
+        !spawn.set_environment_variables.is_empty(),
+    ) {
+        (None, None, false) => None,
+        _ => {
+            let mut builder = spawn
+                .args
+                .as_ref()
+                .map(|args| CommandBuilder::from_argv(args.iter().map(Into::into).collect()))
+                .unwrap_or_else(CommandBuilder::new_default_prog);
+            for (k, v) in spawn.set_environment_variables.iter() {
+                builder.env(k, v);
+            }
+            if let Some(cwd) = &spawn.cwd {
+                builder.cwd(cwd);
+            }
+            Some(builder)
         }
-        if let Some(cwd) = spawn.cwd {
-            builder.cwd(cwd);
-        }
-        Some(builder)
     };
 
     let workspace = mux.active_workspace().clone();
